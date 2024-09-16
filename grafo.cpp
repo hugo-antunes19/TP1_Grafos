331f3be54e5c270ac4267b9710d6439cbdec3f5f
Hugo Antunes <140461883+hugo-antunes19@users.noreply.github.com>
2024-09-16 17:00:09 -0300

    Update grafo.cpp

--- grafo.cpp
+++ grafo.cpp
@@ -2,40 +2,59 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 int main() {
-    // Open the input file
+    // Abrir o arquivo
     std::ifstream inputFile("example.txt");
 
-    // Check if the file is successfully opened
+    // checar se o arquivo está abrindo
     if (!inputFile.is_open()) {
-        std::cerr << "Error opening the file!" << std::endl;
+        std::cerr << "Não conseguimos abrir o arquivo =[" << std::endl;
         return 1;
     }
 
-    // Read the file line by line
+    // ler o arquivo linha por linha
     std::string line;
     int i = 0;
-    while (std::getline(inputFile, line)) {
+    std::vector< int > esq; // o array esq contém todos os números da coluna da esquerda
+    std::vector< int > dir; // o array dir os da direita
+    while (std::getline(inputFile, line)) { // percorrer as linhas do arquivo
 
-        // Primeira linha possui a quantidade de vértices
+        // primeira linha possui a quantidade de vértices
         if (i == 0){
             std::stringstream ss(line);
             int vertices;
             ss >> vertices;
         }
+
+        // esse else e while separam os números do input file
         else{
-            for (size_t i = 0; i < line.length(); ++i) {
-                if (line[i] == ' ' || line[i] == '  '){
-                    std::stringstream ss(line[i]);
-                }
-    }
+            std::string numberStr; // string para armazenar o número em string
+            size_t x = 0;
+        while (x < line.length()) { // percorrer os caracteres das linhas
+            if (line[x] == '\t' || line[x] == ' ') { // teremos apenas duas colunas por linha, separadas por tab ou espaço
+                std::stringstream ss(numberStr); // transformar string em inteiro
+                int number;
+                ss >> number;
+                esq.push_back(number); // colocando o inteiro na lista
+                numberStr = "";
+            }
+            numberStr += line[x];
+            if (x == line.length() - 1) { // a coluna da direita acaba quando i chega no seu valor máximo
+                std::stringstream ss(numberStr);
+                int number;
+                ss >> number;
+                dir.push_back(number);
+                numberStr = "";
+            }
+            x = x+1;
         }
-        std::cout << line << std::endl;
+    }
         i = i+1;
     }
 
-    // Close the file
+    // fechar o arquivo
     inputFile.close();
 
     return 0;
