7320dc377ba13ca490ee8a92cb52a389ee6122c0
Hugo Antunes <140461883+hugo-antunes19@users.noreply.github.com>
2024-09-18 14:06:04 -0300

    Update grafo.cpp

--- grafo.cpp
+++ grafo.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <iostream>
+#include <getopt.h>
 #include <fstream>
 #include <sstream>
 using namespace std;
@@ -23,34 +24,81 @@ void printListAdj(const vector<vector<long long>>& listAdj) {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     // Abrir o arquivo
-    ifstream file("./grafo_1.txt");
+    int opt;
+    string inputFile;
+    string option; // Matriz ou Lista
+    int matriz;
+    int lista;
+    struct option long_options[] = {
+        {"file", required_argument, 0, 'f'},
+        {"option", required_argument, 0, 'o'},
+        {0, 0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "f:o:", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'f':
+                inputFile = optarg;
+                break;
+            case 'o':
+                option = optarg;
+                break;
+        }
+    }
+
+    if (option == "matriz"){
+        matriz = 1;
+    }
+
+    else {
+        lista = 1;
+    }
+
+    if (inputFile.empty() || option.empty()) {
+        cerr << "Uso: " << argv[0] << " --file <inputfile> --option <option>" << endl;
+        return 1;
+    }
+
+    ifstream file(inputFile);
     if (!file.is_open()) {
         cout << "Não conseguimos abrir o arquivo =[" << endl;
         return 1;
     }
-
     long long vertices;
     file >> vertices;
-    vector<vector<long long>> listAdj(vertices+1); // Cria vetor de vetores
-    vector<vector<long long>> matrizAdj(vertices+1, vector<long long>(vertices+1, 0)); // Inicializa a matriz
     long long x, y, arestas = 0;
-
-    while (file >> x >> y) { // Atribui os inteiros das colunas para cada variável (x e y)
+    if (lista ==1){
+        vector<vector<long long>> listAdj(vertices+1); // Cria Lista de Adjacência
+        while (file >> x >> y) { // Atribui os inteiros das colunas para cada variável (x e y)
         if (x <= vertices && y <= vertices) { // Verifica se x e y estão dentro dos limites
             listAdj[x].push_back(y); // Adiciona na Lista de Adjacência
             listAdj[y].push_back(x);
+            arestas++;
+            }
+        }
+        cout << "Lista de Adjacência:" << endl;
+        printListAdj(listAdj);
+    }
+    if (matriz == 1){
+        vector<vector<long long>> matrizAdj(vertices+1, vector<long long>(vertices+1, 0)); // Inicializa a matriz
+
+        while (file >> x >> y) { // Atribui os inteiros das colunas para cada variável (x e y)
+        if (x <= vertices && y <= vertices) { // Verifica se x e y estão dentro dos limites
             matrizAdj[x][y] = 1; // Adiciona na Matriz de Adjacência
             matrizAdj[y][x] = 1;
             arestas++;
+            }
         }
+        cout << "Matriz de Adjacência:" << endl;
+        printMatrizAdj(matrizAdj);
     }
     // Fechar o arquivo
     file.close();
 
-    cout << "Lista de Adjacência:" << endl;
-    printListAdj(listAdj);
-
     return 0;
 }
+
+// g++ seu_codigo.cpp -o seu_programa
+// ./seu_programa -f grafo_1.txt -o matriz (EXEMPLO)
