6e9e3faf56262e8175d68583456e6f06d76c87d1
Hugo Antunes <140461883+hugo-antunes19@users.noreply.github.com>
2024-09-25 20:11:30 -0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -5,21 +5,55 @@
 #include <sstream>
 #include <queue> 
 #include <chrono>
+#include <algorithm>
 #include "graph.h"
 using namespace std;
 
 // Definição do construtor
-Grafo::Grafo(unsigned int v, bool isMatriz) 
-    : vertices(v), usaMatriz(isMatriz), 
-      visitado(vertices + 1, false), 
-      pai(vertices + 1, -1), 
-      nivel(vertices + 1, -1), 
-      n_CompConexa(0) { // Inicializando n_CompConexa
+Grafo::Grafo(const string &grafo, bool isMatriz)
+    : usaMatriz(isMatriz), n_CompConexa(0) {
+
+    ifstream file(grafo);
+    if (!file.is_open()) {
+        cerr << "Erro ao abrir o arquivo!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Lê o número de vértices
+    file >> vertices;
+    
+    visitado.resize(vertices + 1, false);
+    pai.resize(vertices + 1, -1);
+    nivel.resize(vertices + 1, -1);
+    arestas =0;
     if (usaMatriz) {
         matrizAdj.resize(vertices + 1, vector<bool>(vertices + 1, false));
+
+        // Lê as arestas e preenche a matriz de adjacência
+        unsigned int u, v;
+        while (file >> u >> v) {
+            matrizAdj[u][v] = true;
+            matrizAdj[v][u] = true; // Se o grafo for não direcionado
+            arestas++;
+        }
     } else {
         listAdj.resize(vertices + 1);
+
+        // Lê as arestas e preenche a lista de adjacência
+        unsigned int u, v;
+        while (file >> u >> v) {
+            listAdj[u].push_back(v);
+            listAdj[v].push_back(u); // Se o grafo for não direcionado
+            arestas++;
+        }
     }
+
+    file.close();
+    }
+
+// Get vertices
+unsigned int Grafo::getVertices(){
+    return vertices;
 }
 
 void Grafo::adicionarAresta(unsigned int x, unsigned int y) {
@@ -32,34 +66,58 @@ void Grafo::adicionarAresta(unsigned int x, unsigned int y) {
     }
 }
 
-
-void Grafo::graus() {
-    int max = INT_MIN;  // Valor mínimo possível para garantir a atualização
-    int min = INT_MAX;  // Valor máximo possível para garantir a atualização
+// Graus do grafo (máx,min,mediana e média)
+tuple<unsigned int, unsigned int, unsigned int, float> Grafo::graus() {
+    unsigned int max = INT_MIN;  // Valor mínimo possível para garantir a atualização
+    unsigned int min = INT_MAX;  // Valor máximo possível para garantir a atualização
+    unsigned int mediana = 0;
+    float media = 0.0;
     vector<int> graus(vertices + 1, -1);
+    float total = 0.0;
     if (usaMatriz) {
         for (unsigned int i = 0; i < vertices; i++) {
             graus[i] = accumulate(matrizAdj[i].begin(), matrizAdj[i].end(), 0); // Calcula graus
+            total = total + graus[i];
         }
     } else {
         for (unsigned int i = 0; i < vertices; i++) {
             graus[i] = listAdj[i].size(); // Calcula graus na lista de adjacência
+            total = total + graus[i];
         }
     }
-    
+    sort(graus.begin(),graus.end());
     // Encontrar os graus máximo e mínimo
-    for (unsigned int i = 0; i < vertices; i++) {
-        if (graus[i] > max) {
-            max = graus[i];
-        }
-        if (graus[i] < min) {
-            min = graus[i];
-        }
-    }
-    
-    cout << "Grau máximo: " << max << ", Grau mínimo: " << min << endl;
+    max = graus.back();
+    min = graus.front();
+    int size = graus.size();
+    mediana = graus[size/2];
+    media = total/vertices;
+
+    tuple<unsigned int, unsigned int, unsigned int, float> resultado = make_tuple(max,min,mediana,media);
+
+    return resultado;
 }
 
+// Criar txt
+void Grafo::write_general_info(const string &arquivo){
+    tuple<unsigned int, unsigned int, unsigned int, float> t = graus();
+    ofstream outFile(arquivo);
+    if (!outFile.is_open()) {
+        cerr << "Erro ao abrir arquivo de saída." << endl;
+        return;
+    }
+
+    outFile << "Informações gerais do grafo.\n";
+    outFile << "Vértices: " << vertices << endl;
+    outFile << "Arestas: " << arestas << endl;
+    outFile << "Grau máximo: " << get<0>(t) << endl;
+    outFile << "Grau mínimo: " << get<1>(t) << endl;
+    outFile << "Mediana do grau: " << get<2>(t) << endl;
+    outFile << "Média do grau: " << get<3>(t) << endl;
+    
+    outFile.close();
+
+}
 
 // Imprime a Matriz de Adjacência
 void Grafo::printMatrizAdj() const {
@@ -83,7 +141,7 @@ void Grafo::printListAdj() const {
 }
 
 //escrever em arquivo (para retornar a arvore gerada por busca)
-void Grafo::Write_file_busca(vector<unsigned int> pai,vector<unsigned int> nivel, const string& outputFile, unsigned int s){
+void Grafo::Write_file_busca(vector<int> pai,vector<int> nivel, const string& outputFile, unsigned int s){
     ofstream outFile(outputFile);
     if (!outFile.is_open()) {
         cerr << "Erro ao abrir arquivo de saída." << endl;
@@ -111,18 +169,19 @@ vector<unsigned int> Grafo::getVizinhosMatriz(unsigned int v) const {
 
 // Algoritmo Diâmetro aproximado
 int Grafo::aprox(unsigned int start, const string& outputFile, bool write_tree){
+
     BFS (start, outputFile, write_tree); // encontrando a primeira extremidade (x)
     int maior = 0;
     int x;
-    for (int i = 0; i < nivel.size(); i++){
-        if (nivel[i]> maior and nivel[i] != 4294967295){
+    for (int i = 1; i < nivel.size(); i++){
+        if (nivel[i]> maior){
             maior = nivel[i];
             x = i;
         }
     }
     BFS (x, outputFile, write_tree); // encontrando a segunda extremidade começando da primeira extremidade (x)
-    for (int i = 0; i < nivel.size(); i++){
-        if (nivel[i]> maior and nivel[i] != 4294967295){
+    for (int i = 1; i < nivel.size(); i++){
+        if (nivel[i]> maior){
             maior = nivel[i];
             x = i;
         }
@@ -131,26 +190,37 @@ int Grafo::aprox(unsigned int start, const string& outputFile, bool write_tree){
 }
 
 // Algoritmo diâmetro não aproximado
-int Grafo::diameter(const std::string& outputFile, bool write_tree) {
+int Grafo::diameter() {
     int x = 0;
-    int maior = 0; // Inicializa maior com um valor negativo
+    int maior = 0;
 
-    for (int i = 1; i < vertices; i++) {
-        BFS(i, outputFile, 1);
-        for (int j = 0; j < nivel.size(); j++) {
-            if (nivel[j] > maior and nivel[j] != 4294967295 and !nivel.empty()) {
-                maior = nivel[j];
-                x = j; // primeira extremidade
+    for (int s = 1; s < vertices; s++) {
+        visitado.assign(vertices + 1, false);
+        nivel.assign(vertices + 1, -1);
+
+        queue<unsigned int> Q;
+        
+        visitado[s] = true;
+        nivel[s] = 0;
+        Q.push(s);
+
+        while (!Q.empty()) {
+
+            unsigned int v = Q.front();
+            Q.pop();
+
+            const vector<unsigned int>& vizinhos = usaMatriz ? getVizinhosMatriz(v) : listAdj[v];
+            for (unsigned int w : vizinhos) {
+                if (!visitado[w]) {
+                    visitado[w] = true;
+                    nivel[w] = nivel[v] + 1; // Define o nível de w
+                    Q.push(w);
+                    if (nivel[w]>maior){
+                        maior = nivel[w];
+                        x = w;
+                    }
+                }
             }
-        }
-    }
-
-    int ext = 0; // segunda extremidade
-    BFS(x, outputFile, 1);
-    for (int j = 0; j < nivel.size(); j++) {
-        if (nivel[j] > maior and nivel[j] != 4294967295 and !nivel.empty()) {
-            maior = nivel[j];
-            ext = j;
         }
     }
 
@@ -199,6 +269,7 @@ void Grafo::BFS(unsigned int s, const string& outputFile, bool write_tree, unsig
     }
 }
 
+// Algoritmo DFS
 void Grafo::DFS(unsigned int s, const string& outputFile, bool write_tree) {
     visitado.assign(vertices + 1, false);
     pai.assign(vertices + 1, -1);
@@ -232,6 +303,7 @@ void Grafo::DFS(unsigned int s, const string& outputFile, bool write_tree) {
     }
 }
 
+// Algoritmo Componentes Conexas
 map<unsigned int, vector<vector<unsigned int>>, greater<unsigned int>> Grafo::ComponentesConexas() {   
     // Mapa onde a chave é o tamanho da componente e os valores são os vértices da componente
     map<unsigned int, vector<vector<unsigned int>>, greater<unsigned int>> componentesConexas;
@@ -272,6 +344,7 @@ map<unsigned int, vector<vector<unsigned int>>, greater<unsigned int>> Grafo::Co
     return componentesConexas; // Retorna o mapa de componentes
 }
 
+// Imprimir Componentes Conexas
 void Grafo::imprimirComponentesConexas(const map<unsigned int, vector<vector<unsigned int>>, std::greater<unsigned int>>& componentesConexas) {
     if (componentesConexas.empty()) {
         cout << "Nenhuma componente conexa encontrada." << endl;
