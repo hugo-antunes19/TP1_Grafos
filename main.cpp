8295a9e0310b67dba699600ac881ecc7d5957767
Hugo Antunes <140461883+hugo-antunes19@users.noreply.github.com>
2024-09-25 22:47:51 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,33 +7,59 @@
 #include <chrono>
 #include <cstdlib>
 #include <algorithm>
-#include "graph.h"
-// Certifique-se de que você tem o cabeçalho correto
-
+#include <iomanip>
+#include "graph.h" // Certifique-se de que você tem o cabeçalho correto
 using namespace std;
 
+double media_lista_BFS(int vezes, const string &arquivo){
+        Grafo grafo(arquivo,false);
+        int i = 0;
+        double acumulado_bfs = 0;
+        unsigned int acumulado_dfs = 0;
+        while (i<vezes){
+            unsigned int s = std::rand() % grafo.getVertices();
+            auto inicio = std::chrono::high_resolution_clock::now();
+            grafo.BFS(s,"a.txt",0);
+            auto fim = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> duracao = fim - inicio;
+            acumulado_bfs = acumulado_bfs + duracao.count();
+            i++;
+        }
+        return acumulado_bfs;
+    }
+
+double media_lista_DFS(int vezes, const string &arquivo){
+        Grafo grafo(arquivo,false);
+        int i = 0;
+        double acumulado_bfs = 0;
+        unsigned int acumulado_dfs = 0;
+        while (i<vezes){
+            unsigned int s = std::rand() % grafo.getVertices();
+            auto inicio = std::chrono::high_resolution_clock::now();
+            grafo.DFS(s,"a.txt",0);
+            auto fim = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> duracao = fim - inicio;
+            acumulado_bfs = acumulado_bfs + duracao.count();
+            i++;
+        }
+        return acumulado_bfs;
+    } 
+
 int main(int argc, char *argv[]) {
-
-    //Exemplo de como calcular e imprimir componentes conexas (código utilizado para obter os resultados dos experimentos)
-
-    /*
-    Grafo grafo(grafo_1.txt, usaMatriz) // o primeiro termo é alterado de acordo com o grafo que se deseja utilizar
-    
-    map<unsigned int, vector<vector<unsigned int>>, greater<unsigned int>> Comp_do_Grafo = grafo.ComponentesConexas();
-    grafo.imprimirComponentesConexas(Comp_do_Grafo);
-
-    cout << "Numero de Componentes Conexas: " << grafo.n_CompConexa << endl;
-    */
-
-    // Exemplo de chamada para um método que você precisa implementar
-    // grafo.graus(); // Certifique-se de que o método graus() esteja definido
+    Grafo grafo("grafo_1.txt",true);
+    int d = 0;
+    d = grafo.distancia(20,"output.txt",false,30);
+    cout << d << endl;
+    double acumulado_bfs=0;
+    double acumulado_dfs=0;
     for (int i =0;i<11;i++){
-        Grafo grafo("grafo_3.txt",1);
+
         unsigned int s = std::rand() % grafo.getVertices();
         auto inicio = std::chrono::high_resolution_clock::now();
         grafo.BFS(s,"a.txt",0);
         auto fim = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duracao = fim - inicio;
+        acumulado_bfs= acumulado_bfs + duracao.count();
         std::cout << "Tempo de execução BFS: " << duracao.count() << " segundos" << std::endl;
 
         s = std::rand() % grafo.getVertices();
@@ -41,11 +67,13 @@ int main(int argc, char *argv[]) {
         grafo.DFS(s,"a.txt",0);
         fim = std::chrono::high_resolution_clock::now();
         duracao = fim - inicio;
+        acumulado_dfs= acumulado_dfs + duracao.count();
         std::cout << "Tempo de execução DFS: " << duracao.count() << " segundos" << std::endl;
     }
+    cout<< std::setprecision(6) << acumulado_bfs/10 << endl;
+    cout << std::setprecision(10) << acumulado_dfs/10 << endl;
     return 0;
 }
-
 
 // g++ -O3 -o a.exe main.cpp graph.cpp
 // ./a.exe --file grafo_0.txt --useMatrix nao
